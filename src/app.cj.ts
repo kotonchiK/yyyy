7b7f4939e032ab385bd014b0ca808d1737d29515
kotonchiK <anton.mut05@gmail.com>
2024-10-18 21:07:50 +0200

    dasdasd

--- src/app.cj.ts
+++ src/app.cj.ts
@@ -1,24 +1,9 @@
 import {
   Controller,
   Get,
-  Post,
-  Patch,
-  Delete,
-  Body,
-  Param,
-  Query,
-  ParseIntPipe,
-  NotFoundException,
-  HttpStatus,
-  HttpCode,
+
 } from '@nestjs/common';
-import { CreateUserDto } from '../models/input/create.user.model';
-import { UpdateUserDto } from '../models/input/update.user.model';
-import { UserService } from '../../application/services/user.service';
-import { UsersQueryRepository } from '../query-repository/users.query.repo';
-import { validateQueryParameters } from '../../core/query.validator';
-import { ResponseModel } from '../models/output/result.model';
-import { EndpointEnum, RoutingEnum } from '../../core/routes/routes';
+
 
 @Controller()
 export class Appcontroller {
